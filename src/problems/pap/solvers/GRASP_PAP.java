3149c753c1fc0431961d6997b427f8296e167b4d
VFerrari <vferrari@mpc.com.br>
2020-12-24 03:41:28 -0300

    Adding viability check function to util script

--- src/problems/pap/solvers/GRASP_PAP.java
+++ src/problems/pap/solvers/GRASP_PAP.java
@@ -378,7 +378,7 @@ public class GRASP_PAP extends AbstractGRASP<Integer[]> {
 		// Changeable parameters.
 		double alpha1 = 0.25, alpha2 = 0.7;
 		
-		FileWriter fileWriter = new FileWriter("results/GRASP_PAP.txt");
+		FileWriter fileWriter = new FileWriter("results/GRASP_PAP_DEBUG.txt");
 		GRASP_PAP.run(alpha1, maxIterations, "instances/" + "P50D50S1.pap", 
 					  BiasFunction.RANDOM,  Construction.DEF, rpgP, maxTime,
 					  fileWriter);
---
+++ src/util.py
@@ -0,0 +1,122 @@
+
+from sys import argv
+from os.path import exists
+import numpy as np
+
+# Example for reading instance.
+def read_instance(filename):    
+    PAP = {}
+    
+    # Read
+    with open(filename, 'r') as fp:
+        data = fp.readlines()
+        
+    # Scalars
+    P = PAP['P'] = int(data[0][2:-1])
+    D = PAP['D'] = int(data[1][2:-1])
+    PAP['T'] = int(data[2][2:-1]) # NOTE: unnecessary, always 20
+    PAP['S'] = int(data[3][2:-1])
+    PAP['H'] = int(data[4][2:-1]) # NOTE: unnecessary, always 3
+    
+    # Advance
+    data = data[6:]
+    
+    # Array
+    h = np.empty(D, np.uint8)
+    for i in range(D):
+        h[i] = int(data[i][:-1])
+    PAP['h'] = h
+    
+    # Advance
+    data = data[D+1:]
+    
+    # 2D arrays
+    a = data[:P]
+    for i in range(P):
+        a[i] = a[i][:-1].split("\t")
+    PAP['a'] = np.array(a, np.uint8)
+    
+    # Advance and finish
+    r = data[P+1:]
+    for i in range(P):
+        r[i] = r[i][:-1].split("\t")
+    PAP['r'] = np.array(r, np.uint8)
+    
+    return PAP
+    
+def read_solution(filename):
+    sol = []
+    
+    # Read
+    with open(filename, 'r') as fp:
+        data = fp.readlines()
+    
+    data=data[1:]
+    for lin in data:
+        lin = lin[1:-2].split(",")
+        sol.append(tuple(map(int, lin)))
+        
+    return sol
+
+def sol_to_vars(inst, sol):
+    x = np.zeros((inst['P'], inst['D']))
+    z = np.zeros((inst['P'], inst['T']))
+    
+    for s in sol:
+        p,d,t = s
+        x[p][d] = 1
+        z[p][t] = 1
+    
+    y = x.sum(axis=0)
+    
+    return x,y,z
+
+def check_viability(inst, sol):
+    x,y,z = sol_to_vars(inst, sol)
+    
+    # Restriction 2.3
+    if not (z.sum(axis=0) <= inst['S']).all():
+        print("Class violation! Infeasible.")
+        print("Rooms available:", inst['S'])
+        print("Classes per slot:")
+        print(z.sum(axis=0))
+        return
+
+    # Restriction 2.4
+    if not (z <= inst['r']).all():
+        print("Availability violation! Infeasible.")
+        where = np.where(z > inst['r'])
+        print(list(zip(where[0], where[1])))
+        return
+    
+    # Restriction 2.5
+    if not (z.sum(axis=1) <= inst['H']).all():
+        print("Workload violation! Infeasible.")
+        print("Max workload:", inst['H'])
+        print("Actual workload per prof:")
+        print(z.sum(axis=1))
+        return
+    
+    # Restriction 2.6
+    if not (z.sum(axis=1) == inst['h'] * x.sum(axis=1)).all():
+        print("Coupling violation! Infeasible.")
+        print("Slots per professor:")
+        print(z.sum(axis=1))
+        
+        print("Professor workload:")
+        print(inst['h'] * x.sum(axis=1))
+        return
+    
+    print("Solution is feasible!")
+
+if __name__ == "__main__":
+    
+    # Valid argument
+    assert len(argv) == 3, "Please insert both instance and solution files, in that order."
+    assert exists(argv[1]), "Instance does not exist!"
+    assert exists(argv[2]), "Solution does not exist!"
+    
+    PAP = read_instance(argv[1])
+    sol = read_solution(argv[2])
+    
+    check_viability(PAP, sol)
